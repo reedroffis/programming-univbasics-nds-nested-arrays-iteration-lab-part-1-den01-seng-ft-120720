a3bd3ce07534f250b690719d4fc0d2ba94278883
Reed Roffis <reedroffis18@gmail.com>
2020-12-01 21:45:57 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,8 +6,8 @@ def find_even_values(src)
   while row_index < src.count do
     element_index = 0
     while element_index < src[row_index].count do
-      
-      p src[row_index][element_index]
+      src[row_index][element_index]
+      p 
       element_index += 1
     end
     row_index += 1
